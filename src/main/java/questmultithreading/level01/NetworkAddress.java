14405238fab42ddca49c53679a67be3172e5d787
Vasiliy Shaitan <shaitan@kraud.ru>
2021-03-15 16:59:39 +0300

    Add network address

---
+++ src/main/java/questmultithreading/level01/NetworkAddress.java
@@ -0,0 +1,32 @@
+package questmultithreading.level01;
+
+
+/*
+Определяем адрес сети
+*/
+public class NetworkAddress {
+    public static void main(String[] args) {
+        byte[] ip = new byte[]{(byte) 192, (byte) 168, 1, 2};
+        byte[] mask = new byte[]{(byte) 255, (byte) 255, (byte) 254, 0};
+        byte[] netAddress = getNetAddress(ip, mask);
+        print(ip);          //11000000 10101000 00000001 00000010
+        print(mask);        //11111111 11111111 11111110 00000000
+        print(netAddress);  //11000000 10101000 00000000 00000000
+    }
+
+    public static byte[] getNetAddress(byte[] ip, byte[] mask) {
+        byte[] netAddress = new byte[4];
+        for (int i = 0; i < ip.length; i++) {
+            byte b = (byte) (ip[i] & mask[i]);
+            netAddress[i] = b;
+        }
+        return netAddress;
+    }
+
+    public static void print(byte[] bytes) {
+        for (byte aByte : bytes) {
+            System.out.print(String.format("%8s", Integer.toBinaryString(aByte & 0xFF)).replace(' ', '0') + " ");
+        }
+        System.out.println();
+    }
+}
